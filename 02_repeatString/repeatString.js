c228840e8c772c5d56b6c4a97a7647641a877739
gnve <5559655+gnve@users.noreply.github.com>
2025-02-17 16:12:23 -0800

    Solve 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let thisString = "";
+    for (i = 0; i < num; i++) {
+        thisString += string;
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    return thisString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    input.split('').reverse().join('');
 };
 
 // Do not edit below this line
